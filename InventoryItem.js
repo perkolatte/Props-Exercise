b48bc96223215103682debe6e4a358f04ab7d952
Ian Isaacson <ian_isaacson@yahoo.com>
2026-01-08 21:17:59 -0800

    feat: render inventory items with InventoryItem
    - map inventory list to InventoryItem entries in App.js
    - destructure InventoryItem props with defaults and render details
    - convert placeholder comments to JSX comments

--- InventoryItem.js
+++ InventoryItem.js
@@ -1,10 +1,17 @@
-function InventoryItem (/* TODO: Take the props. Set defaults to the quantity. */)
-{
-	return (
-		<div>
-			<h2><!-- TODO: Render the item's details. --></h2>
-			<!-- TODO: Render the low stock alert based on the quantity of the item. -->
-			<!-- TODO: Render the high value alert based on the total value of the item. -->
-		</div>
-	);
+function InventoryItem({
+  name = "unknown",
+  type = "unknown",
+  quantity = 0,
+  price = "unknown",
+}) {
+  return (
+    <div>
+      <h2>
+        {name} ({type})
+      </h2>
+      <p>{`${quantity} remained.`}</p>
+      {/*<!-- TODO: Render the low stock alert based on the quantity of the item. --> */}
+      {/*<!-- TODO: Render the high value alert based on the total value of the item. --> */}
+    </div>
+  );
 }
